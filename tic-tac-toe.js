7ec1bc9f5d7bc0b983eaefb730d34e003d639258
Kimberly Pecco <Kim.pecco2.0@gmail.com>
2023-10-22 18:45:31 -0500

    Check for winner added to JAVAscript

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,19 +1,48 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     const board = document.getElementById("board");
-    const gameState = [];
+    const gameState = Array(9).fill(""); 
     let currentPlayer = "X";
+    let gameEnded = false; 
+  
+
+    const winCombinations = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+  
+    const status = document.getElementById("status");
   
     for (let i = 0; i < board.children.length; i++) {
       const square = board.children[i];
       square.classList.add("square");
   
       square.addEventListener("click", () => {
-        if (!gameState.includes("X") && !gameState.includes("O")) {
-          if (!square.classList.contains("X") && !square.classList.contains("O")) {
-            square.classList.add(currentPlayer);
-            currentPlayer = currentPlayer === "X" ? "O" : "X";
-            gameState.push(square.classList.contains("X") ? "X" : "O");
+        if (!gameState[i] && !gameEnded) {
+          square.classList.add(currentPlayer);
+          gameState[i] = currentPlayer;
+          square.innerHTML = currentPlayer;
+  
+          for (const combination of winCombinations) {
+            const [a, b, c] = combination;
+            if (
+              gameState[a] === currentPlayer &&
+              gameState[b] === currentPlayer &&
+              gameState[c] === currentPlayer
+            ) {
+              gameEnded = true;
+              status.innerHTML = `Congratulations! ${currentPlayer} is the Winner!`;
+              status.classList.add("you-won");
+            }
           }
+  
+         
+          currentPlayer = currentPlayer === "X" ? "O" : "X";
         }
       });
   
