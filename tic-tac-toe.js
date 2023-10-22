e36f660083533cba11b75d0f096b8945442d3faa
Kimberly Pecco <Kim.pecco2.0@gmail.com>
2023-10-22 18:24:55 -0500

    Hover added to JAVAscript

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,12 +3,10 @@ document.addEventListener("DOMContentLoaded", (event) => {
     const gameState = [];
     let currentPlayer = "X";
   
-   
     for (let i = 0; i < board.children.length; i++) {
       const square = board.children[i];
       square.classList.add("square");
   
-    
       square.addEventListener("click", () => {
         if (!gameState.includes("X") && !gameState.includes("O")) {
           if (!square.classList.contains("X") && !square.classList.contains("O")) {
@@ -17,6 +15,14 @@ document.addEventListener("DOMContentLoaded", (event) => {
             gameState.push(square.classList.contains("X") ? "X" : "O");
           }
         }
+      });
+  
+      square.addEventListener("mouseover", () => {
+        square.classList.add("hover");
+      });
+  
+      square.addEventListener("mouseleave", () => {
+        square.classList.remove("hover");
       });
     }
   });
