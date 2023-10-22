3a6d47cfc3c4d8f504b0f47ed239496ce490006e
Kimberly Pecco <Kim.pecco2.0@gmail.com>
2023-10-22 17:48:35 -0500

    Update JAVAscript

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,20 +1,16 @@
-/*document.querySelector("html").addEventListener('click', () => {
-    alert("X")
-});
-*/
-
-// document.addEventListener("DOMContentLoaded", (event) => {
-//     const squares = document.querySelector(".square");
-
-//     squares.forEach(square =>{
-//         square.classList.add("box")
-//     })
-// })
-
-document.addEventListener("DOMContentLoaded", (event) => {
-    board = document.getElementById("board");
-
-    for(let i = 0; i < board.children.length; i++) {
-        board.children[i].classList.add("square");
-    }
-});
+document.addEventListener("DOMContentLoaded", () => {
+    const boardCells = document.querySelectorAll(".square");
+    const gameState = [];
+    let currentPlayer = "X";
+  
+    boardCells.forEach((cell) => {
+      cell.addEventListener("click", () => {
+        if (!cell.classList.contains("X") && !cell.classList.contains("O")) {
+          cell.classList.add(currentPlayer);
+          currentPlayer = currentPlayer === "X" ? "O" : "X";
+          gameState.push(cell.classList.contains("X") ? "X" : "O");
+        }
+      });
+    });
+  });
+  
