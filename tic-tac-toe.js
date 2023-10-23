0bf8b3ca41603b6ebb75a76625876098dbf39949
Kimberly Pecco <Kim.pecco2.0@gmail.com>
2023-10-22 19:19:30 -0500

    Reset button added to JAVAscript

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", (event) => {
     const board = document.getElementById("board");
     const gameState = Array(9).fill(""); 
     let currentPlayer = "X";
-    let gameEnded = false; 
-  
+    let gameEnded = false;
 
+    
     const winCombinations = [
       [0, 1, 2],
       [3, 4, 5],
@@ -17,6 +17,25 @@ document.addEventListener("DOMContentLoaded", (event) => {
     ];
   
     const status = document.getElementById("status");
+    const newGameButton = document.getElementById("newGameButton");
+
+    function resetGame() {
+      for (let i = 0; i < gameState.length; i++) {
+        gameState[i] = "";
+        const square = board.children[i];
+        square.classList.remove("X", "O");
+        square.innerHTML = "";
+      }
+  
+      status.innerHTML = "Move your mouse over a square and click to play an X or an O.";
+      status.classList.remove("you-won");
+      currentPlayer = "X";
+      gameEnded = false;
+    }
+  
+    newGameButton.addEventListener("click", () => {
+      resetGame();
+    });
   
     for (let i = 0; i < board.children.length; i++) {
       const square = board.children[i];
@@ -40,8 +59,6 @@ document.addEventListener("DOMContentLoaded", (event) => {
               status.classList.add("you-won");
             }
           }
-  
-         
           currentPlayer = currentPlayer === "X" ? "O" : "X";
         }
       });
